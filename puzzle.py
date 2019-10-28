be40da9306910524db41b5f9c32eed158735f3a6
Changle Xie <xcl5314@gmail.com>
2019-10-28 16:18:02 +0800

    Optimized program

--- puzzle.py
+++ puzzle.py
@@ -2,8 +2,9 @@ from tkinter import Frame, Label, CENTER, messagebox
 import constants as c
 import logic as l
 import json
-import time
 import threading
+import random
+import time
 
 
 class Game(Frame):
@@ -46,15 +47,21 @@ class Game(Frame):
         self.update_grid()
     
     def init_matrix(self):
-        
-        with open('gamestate.text', 'r', encoding='utf-8') as f:
-            t = f.read()
-            if not t:
-                self.matrix = l.new_game(c.GRID_LEN)
-                self.matrix = l.gen(self.matrix)
-                self.matrix = l.gen(self.matrix)
-            else:
-                self.matrix = json.loads(t)
+        box = messagebox.askquestion('Start a new game?', 'Sure?')
+        if box == 'yes':
+            self.matrix = l.new_game(c.GRID_LEN)
+            self.matrix = l.gen(self.matrix)
+            self.matrix = l.gen(self.matrix)
+        else:
+            
+            with open('gamestate.text', 'r', encoding='utf-8') as f:
+                t = f.read()
+                if not t:
+                    self.matrix = l.new_game(c.GRID_LEN)
+                    self.matrix = l.gen(self.matrix)
+                    self.matrix = l.gen(self.matrix)
+                else:
+                    self.matrix = json.loads(t)
     
     def update_grid(self):
         for i in range(c.GRID_LEN):
@@ -103,8 +110,8 @@ class Game(Frame):
                     self.grid_rows[1][2].configure(text='Win', bg=c.BACKGROUND_EMPTY_CELL_COLOR)
                     c.winnum *= 2
                 if l.game_state(self.matrix, c.winnum) == 'lose':
-                    self.grid_rows[1][1].configure(text='You', bg=c.BACKGROUND_EMPTY_CELL_COLOR)
-                    self.grid_rows[1][2].configure(text='lose', bg=c.BACKGROUND_EMPTY_CELL_COLOR)
+                    # self.grid_rows[1][1].configure(text='You', bg=c.BACKGROUND_EMPTY_CELL_COLOR)
+                    # self.grid_rows[1][2].configure(text='lose', bg=c.BACKGROUND_EMPTY_CELL_COLOR)
                     box = messagebox.askquestion('Restart?', 'Sure?')
                     if box == 'yes':
                         self.restart()
@@ -122,7 +129,6 @@ class Game(Frame):
             for i in 'dsd':
                 self.auto_key_down(i)
                 self.pprev, self.prev = self.prev, self.matrix
-                
     
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -134,10 +140,100 @@ class Game(Frame):
 class Control:
     def __init__(self, g):
         self.g = g
+        self.pprev, self.prev = [], [0]
+        self.direction = 1
+        self.prec = ''
     
     def auto_run(self):
         while True:
-            self.g.auto_run()
+            self.runs()
+    
+    def runs(self):
+        if self.g.matrix[-1][-1] == 0:
+            for i in 'sd':
+                self.key_down(i)
+        # if self.prev != self.pprev:
+        #     t = self.predict(self.g.matrix)
+        #     self.key_down(t)
+        #     self.pprev, self.prev = self.prev, self.g.matrix
+        # else:
+        #     self.key_down('s')
+        #     self.pprev, self.prev = self.prev, self.g.matrix
+        #     self.key_down('d')
+        #     self.pprev, self.prev = self.prev, self.g.matrix
+        #     if self.prev == self.pprev:
+        #         self.key_down('u')
+        #         self.pprev, self.prev = self.prev, self.g.matrix
+        #         if self.prev == self.pprev:
+        #             self.key_down('a')
+        #             self.pprev, self.prev = self.prev, self.g.matrix
+        #             self.key_down('d')
+        #             self.pprev, self.prev = self.prev, self.g.matrix
+    
+        if self.prev == self.pprev:
+            self.key_down('w')
+            self.pprev, self.prev = self.prev, self.g.matrix
+            if self.prev == self.pprev:
+                self.key_down('a')
+                self.pprev, self.prev = self.prev, self.g.matrix
+        else:
+            t = self.predict(self.g.matrix)
+            for i in t:
+                self.key_down(i)
+                self.pprev, self.prev = self.prev, self.g.matrix
+     
+    
+    def key_down(self, key):
+        return self.g.auto_key_down(key)
+    
+    def next_step(self, mat):
+        temp = l.transpose(mat)
+    
+    def predict(self, mat):
+        w = l.up(mat)[0]
+        d = l.right(mat)[0]
+        
+        if mat[-1][-1] == 0:
+            return 's'
+
+        d0 = sum([b.count(0) for b in d])
+        m0 = sum([b.count(0) for b in mat])
+        if d0 > m0:
+            return 'd'
+        
+        for i in range(len(w)):
+            for j in range(len(w[0])-1):
+                if w[i][j] == w[i][j+1]:
+                    return 's'
+            
+        
+        for i in range(len(w)):
+            for j in range(len(w)-1):
+                if w[i][j] == w[i][j+1]:
+                    return 'wd'
+        
+        
+        # code = ['w', 's', 'a', 'd']
+        # for j in range(c.GRID_LEN - 1, -1, -2):
+        #     for i in range(c.GRID_LEN - 1, -1, -1):
+        #         t = self.compare(w[i][j], s[i][j], a[i][j], d[i][j])
+        #         if t:
+        #             temp = [w[i][j], s[i][j], a[i][j], d[i][j]]
+        #             return code[temp.index(max(temp))]
+        #
+        #     for i in range(c.GRID_LEN):
+        #         t = self.compare(w[i][j - 1], s[i][j - 1], a[i][j - 1], d[i][j - 1])
+        #         if t:
+        #             temp = [w[i][j - 1], s[i][j - 1], a[i][j - 1], d[i][j - 1]]
+        #             return code[temp.index(max(temp))]
+        
+        return 'sd'
+    
+    def compare(self, w, s, a, d):
+        l = [w, a, s, d]
+        if l.count(max(l)) == 1:
+            return True
+        return False
 
 
 game_grid = Game()
